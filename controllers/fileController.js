f2effa872aa862fa9721eab62f09ec5d7b2b3cdb
eladex <eladex@gmail.com>
2016-10-04 21:13:15 +0300

    file upload demo

--- controllers/fileController.js
+++ controllers/fileController.js
@@ -1,8 +1,20 @@
 var Files = require('../models/fileModel');
 // var _ = require('underscore');
 var bodyParser = require('body-parser');
+var cors = require('./cors.js');
+
+var multer = require('multer');
+var storage = multer.diskStorage({
+    destination: '/home/eladex/Desktop/tmp/',
+    filename: function(req, file, cb){
+        cb(null, file.originalname);
+    }
+
+});
+var upload = multer({storage: storage}).any();
 
 module.exports = function(app){
+	app.use(cors());	
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -13,5 +25,16 @@ module.exports = function(app){
             var normalizedFile = file.normalize();
             res.send(normalizedFile);
         })
-    })
+    });
+
+    app.post('/api/file/upload', function(req,res){
+        upload(req, res, function(err){
+            if(err){
+                console.log(err);
+                return res.status(500).end();
+            }
+            console.log(req.body);
+            res.status(200).end();
+        });   
+    });
 }
